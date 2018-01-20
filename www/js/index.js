cc3d2161f1965c5e888d9ceae6b2cee5de721c7a
patricioarango <patricioarango@gmail.com>
2018-01-20 13:57:51 -0300

    two

--- www/js/index.js
+++ www/js/index.js
@@ -76,3 +76,36 @@ connectedRef.on("value", function(snap) {
     conexion = false;
 }
 });
+
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                console.log("esta logueado");
+                console.log(user);
+            } else {
+                console.log("no logueado");
+                firebaselogin();
+            }
+        }); 
+
+
+        function firebaselogin(){
+
+    var provider = new firebase.auth.GoogleAuthProvider(); 
+    firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+      //app.show_user(user);
+  } else {
+    firebase.auth().signInWithRedirect(provider);
+  }
+}).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+});
+            
+        }
