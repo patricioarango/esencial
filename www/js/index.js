1b3566cd18f15c90f080533cdf9cbc609913f88f
patricioarango <patricioarango@gmail.com>
2018-01-20 14:23:15 -0300

    two

--- www/js/index.js
+++ www/js/index.js
@@ -77,35 +77,22 @@ connectedRef.on("value", function(snap) {
 }
 });
 
-        firebase.auth().onAuthStateChanged(function(user) {
-            if (user) {
-                console.log("esta logueado");
-                console.log(user);
-            } else {
-                console.log("no logueado");
-                firebaselogin();
-            }
-        }); 
+var provider = new firebase.auth.GoogleAuthProvider();
 
-
-        function firebaselogin(){
-
-    var provider = new firebase.auth.GoogleAuthProvider(); 
-    firebase.auth().getRedirectResult().then(function(result) {
-        if (result.credential) {
-      //app.show_user(user);
-  } else {
-    firebase.auth().signInWithRedirect(provider);
-  }
+firebase.auth().signInWithRedirect(provider).then(function() {
+  return firebase.auth().getRedirectResult();
+}).then(function(result) {
+  // This gives you a Google Access Token.
+  // You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  console.log(user);
+  // ...
 }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
 });
-            
-        }
+
+
