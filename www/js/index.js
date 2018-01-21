0294b21faad5f04ec8d7c8654ef55ae42a90aba6
patricioarango <patricioarango@gmail.com>
2018-01-21 10:54:44 -0300

    two

--- www/js/index.js
+++ www/js/index.js
@@ -78,28 +78,25 @@ connectedRef.on("value", function(snap) {
 });
 
 var user = firebase.auth().currentUser;
-
+console.log(user);
 if (user) {
   console.log(user);
 } else {
-                console.log("no logueado");
-               var provider = new firebase.auth.GoogleAuthProvider();
-
-firebase.auth().signInWithRedirect(provider).then(function() {
-  return firebase.auth().getRedirectResult();
-}).then(function(result) {
-  // This gives you a Google Access Token.
-  // You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  console.log(user);
-  // ...
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token.
+    // You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }
 }).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
 });
+              
 }
 
 
