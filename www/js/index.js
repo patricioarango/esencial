36ff4de928c5b5e21f5ecc8a9dc7a1094d6dc6ad
patricioarango <patricioarango@gmail.com>
2018-01-21 11:58:57 -0300

    two

--- www/js/index.js
+++ www/js/index.js
@@ -81,65 +81,17 @@ connectedRef.on("value", function(snap) {
 }
 });
 
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Google Access Token.
-    // You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    console.log("suerefr");
-    console.log(user);
-  }
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-});
 
 
-var user = firebase.auth().currentUser;
-console.log(user);
-if (user) {
-  console.log(user);
-} else {
-    console.log("user else");
-              
-}
 
-        firebase.auth().onAuthStateChanged(function(user) {
-            if (user) {
-                console.log("esta logueado");
-                console.log(user);
-            } else {
-                console.log("no logueado");
-                
-            }
-        }); 
 
-app.buscador = () => {
-    var provider = new firebase.auth.GoogleAuthProvider();
 
-firebase.auth().signInWithRedirect(provider).then(function() {
-  return firebase.auth().getRedirectResult();
-}).then(function(result) {
-  // This gives you a Google Access Token.
-  // You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-});
-}
+
 
 
 
     document.getElementById('search').addEventListener('click', function() {
-        app.buscador();
+        console.log("aca");
     });
 
 
